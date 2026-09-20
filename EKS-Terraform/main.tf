66d1e8cfa39e4c965d592338a120458b7801eca7
murthychiluka <100435093+murthychiluka@users.noreply.github.com>
2026-09-20 23:16:57 +0530

    Update main.tf

--- EKS-Terraform/main.tf
+++ EKS-Terraform/main.tf
@@ -235,7 +235,7 @@ resource "aws_iam_role_policy_attachment" "ecr" {
 
 resource "aws_eks_cluster" "eks" {
 
-  name     = "naresh"
+  name     = "murthy"
   role_arn = aws_iam_role.cluster_role.arn
   version  = var.cluster_version
 
